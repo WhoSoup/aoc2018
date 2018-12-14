94b975ee174fcc7d4a46d2b2368e0c1cb509cd40
Who Soup <who.soup@gmail.com>
2018-12-14 12:47:13 +0100

    cleaner

--- day14/day14.go
+++ day14/day14.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 type cookbook struct {
-	recipes []int
-	size    int
-	a, b    int
+	recipes    []int
+	size       int
+	elf, zwölf int
 }
 
 func (cb *cookbook) cook() {
-	new := cb.recipes[cb.a] + cb.recipes[cb.b]
+	new := cb.recipes[cb.elf] + cb.recipes[cb.zwölf]
 
 	if len(cb.recipes) <= cb.size+2 {
 		bigger := make([]int, len(cb.recipes)*2)
@@ -28,47 +27,49 @@ func (cb *cookbook) cook() {
 		cb.recipes[cb.size] = new
 		cb.size++
 	}
-	cb.a = (cb.a + 1 + cb.recipes[cb.a]) % cb.size
-	cb.b = (cb.b + 1 + cb.recipes[cb.b]) % cb.size
-}
-
-func (cb *cookbook) nums(at, count int) (a int) {
-	for i := 0; i < count && at-i >= 0; i++ {
-		a += cb.recipes[at-i] * int(math.Pow10(i))
-	}
-	return
-}
-
-func (cb *cookbook) check(target int) bool {
-	if target == cb.nums(cb.size-1, 6) {
-		fmt.Println("Target found at:", cb.size-6)
-		return true
-	} else if target == cb.nums(cb.size-2, 6) {
-		fmt.Println("Target found at:", cb.size-1-6)
-		return true
-	}
-	return false
+	cb.elf = (cb.elf + 1 + cb.recipes[cb.elf]) % cb.size
+	cb.zwölf = (cb.zwölf + 1 + cb.recipes[cb.zwölf]) % cb.size
 }
 
 func main() {
 	input := 236021
+	target := []int{2, 3, 6, 0, 2, 1}
 
-	cb := cookbook{make([]int, input+10), 2, 0, 1}
+	cb := cookbook{make([]int, input), 2, 0, 1}
 	cb.recipes[0] = 3
 	cb.recipes[1] = 7
 
-	once := true
+	i := 0
+
+Kitchen:
 	for {
 		cb.cook()
-		if once && cb.size >= input+10 {
-			fmt.Println("Part One:", cb.nums(input-1+10, 10))
-			once = false
-		}
 
-		ok := cb.check(input)
+		for ; i <= cb.size; i++ { // i runs once or twice, depending on how many recipes were added
+			if i == input+10 {
+				fmt.Print("Part One:")
+				for j := input; j < i; j++ {
+					fmt.Print(cb.recipes[j])
+				}
+				fmt.Println()
+			}
 
-		if ok {
-			break
+			if i < len(target) {
+				continue
+			}
+
+			m := true
+			for x := 0; x < len(target); x++ {
+				if target[x] != cb.recipes[i-len(target)+x] {
+					m = false
+					break
+				}
+			}
+
+			if m {
+				fmt.Println("Part Two:", i-len(target))
+				break Kitchen
+			}
 		}
 	}
 }
